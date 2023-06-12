35e9e91a2346f6acbbdd1d63028adb748ba58a03
ayan-kundu <75118735+ayan-kundu@users.noreply.github.com>
2023-06-12 19:28:02 +0100

    Update servo.py

--- servo.py
+++ servo.py
@@ -12,6 +12,10 @@ GPIO.setup(12, GPIO.OUT)
 pwm=GPIO.PWM(11, 50)
 pwm.start(0)
 
+#pwm.ChangeDutyCycle(5) # left -90 deg position # sleep(1)
+#pwm.ChangeDutyCycle(7.5) # neutral position # sleep(1)
+#pwm.ChangeDutyCycle(10) # right +90 deg position #sleep(1)
+
 #Function to convert angle to DutyCycle
 def setAngle(angle,pin):
     duty = angle / 18 + 2
